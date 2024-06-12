2da30f7ad73905b253b1029cf352daef1dfbc7d8
Parth Goel <goelparth5331@gmail.com>
2024-06-12 16:22:17 +0530

    add tailwind basic config and Auth Routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+
+
+// https://stackoverflow.com/questions/78255009/react-router-v6-route-without-a-path  great article to refer to
+
+//we can make parent routes without path to give them outlet property and make multiple layouts 
+
+const router = createBrowserRouter([
+    {  //public routes
+        element : < AuthLandingLayout />,
+        children :[ 
+            {
+             path : '/signIn',
+             element : < SignInForm />
+            },
+            {
+                path : '/signUp',
+                element : < SignUpForm />
+            }
+    ]
+    },
+    { //secure Routes 
+        element : < RootLayout />,
+        children : [
+            {
+
+            }
+        ]
+
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+
     <App />
-  </React.StrictMode>,
+  
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const defaultTheme = require('tailwindcss/defaultTheme')
 
+module.exports = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      
+      },
+    },
+    extend: {
+    
+      screens: {
+        'xs': '480px',
+      
+      },
+    
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+
+      },
+    
+  },
+  plugins: [require('tailwindcss-animate')],
+} };
