330341e7990fc80b7229954968ed59b0972e7f0f
Parth Goel <goelparth5331@gmail.com>
2024-06-19 09:30:31 +0530

    userContext finally completed

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { signUpSchema } from "../Utils/Zod.ts";
 import z from "zod";
+import type { TUser } from "../type.d.ts";
+
 
 type TSignUpFormData = z.infer< typeof signUpSchema >
 
@@ -21,8 +23,24 @@ export const signIn = async (data : {
         const response = await axios.post(`${import.meta.env.VITE_USER_URL}/login`, data);
         return await response.data;
     }catch(err){
-        console.log(err);
         throw err;
     }
 }
 
+export const getUser = async () : Promise<TUser>=> {
+   try{
+         const response = await axios.get(`${import.meta.env.VITE_USER_URL}/getUser`);
+         if(response.status === 403){
+            //i will make a another request to get new accesstoken
+            const res = await axios.post(`${import.meta.env.VITE_USER_URL}/getNewAccessToken`);
+            if(res.status === 200){
+                return await axios.get(`${import.meta.env.VITE_USER_URL}/getUser`);
+            }else{
+                throw new Error("Unauthorized");
+            }
+         }
+         return await response.data;
+   }catch(err){
+         throw err;
+   }
+}
